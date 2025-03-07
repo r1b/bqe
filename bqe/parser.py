87a5730114d2a6c1e7e1e772facb814b9d8b0c11
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-07 14:17:12 -0500

    init

---
+++ bqe/parser.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from lark import Lark
+from .settings import PACKAGE_ROOT
+
+GRAMMAR_PATH = PACKAGE_ROOT / Path("grammar/bigquery.lark")
+
+
+def get_parser() -> Lark:
+    return Lark(GRAMMAR_PATH.read_text())
---
+++ tests/test_parser.py
@@ -0,0 +1,36 @@
+import pytest
+
+from bqe.parser import get_parser
+
+
+@pytest.fixture
+def parser():
+    return get_parser()
+
+
+@pytest.mark.parametrize("sql", (
+    pytest.param("SELECT 1", id="select_literal"),
+    pytest.param("(SELECT 1)", id="query_expr"),
+    pytest.param("(SELECT (SELECT (SELECT 1)))", id="query_expr_nested"),
+    pytest.param("SELECT column1 FROM table1", id="select_column_expr_single"),
+    pytest.param("SELECT column1, column2 FROM table1", id="select_column_expr_multi"),
+    pytest.param("SELECT column1, column2 FROM table1, table2", id="select_from_multi"),
+    pytest.param("SELECT column1 AS alias1 FROM table1", id="select_column_expr_alias_explicit"),
+    pytest.param("SELECT column1 alias1 FROM table1", id="select_column_expr_alias_implicit"),
+    pytest.param("SELECT column1 FROM table1 AS alias1", id="select_from_alias_explicit"),
+    pytest.param("SELECT column1 FROM table1 alias1", id="select_from_alias_implicit"),
+    pytest.param("SELECT * FROM table1", id="select_star"),
+    pytest.param("SELECT column1.* FROM table1", id="select_star_scoped"),
+    pytest.param("SELECT column1.column2.* FROM table1", id="select_star_scoped_nested"),
+    pytest.param("SELECT * EXCEPT (column1) FROM table1", id="select_star_except_single"),
+    pytest.param("SELECT * EXCEPT (column1, column2) FROM table1", id="select_star_except_multi"),
+    pytest.param("SELECT * REPLACE (column1 AS alias1) FROM table1", id="select_star_replace_single"),
+    pytest.param("SELECT * REPLACE (column1 AS alias2, column2 AS alias2) FROM table1", id="select_star_replace_multi"),
+    pytest.param("SELECT ALL column1 FROM table1", id="select_mode_all"),
+    pytest.param("SELECT DISTINCT column1 FROM table1", id="select_mode_distinct"),
+    pytest.param("SELECT column1 FROM (SELECT column1 FROM table1)", id="select_from_subquery"),
+    pytest.param("SELECT column1 FROM (SELECT column1 FROM table1) AS table2", id="select_from_subquery_aliased"),
+))
+def test_select_ok(sql, parser, snapshot):
+    tree = parser.parse(sql)
+    assert snapshot == tree.pretty()
