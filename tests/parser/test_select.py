060ae906c9dfa9d2c22c3fc4790a4fe81773b928
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-07 17:01:49 -0500

    chore: add more select tests

--- tests/parser/test_select.py
+++ tests/parser/test_select.py
@@ -14,12 +14,24 @@ import pytest
     pytest.param("SELECT * EXCEPT (column1) FROM table1", id="select_star_except_single"),
     pytest.param("SELECT * EXCEPT (column1, column2) FROM table1", id="select_star_except_multi"),
     pytest.param("SELECT * REPLACE (column1 AS alias1) FROM table1", id="select_star_replace_single"),
-    pytest.param("SELECT * REPLACE (column1 AS alias2, column2 AS alias2) FROM table1", id="select_star_replace_multi"),
+    pytest.param("SELECT * REPLACE (column1 AS alias1, column2 AS alias2) FROM table1", id="select_star_replace_multi"),
     pytest.param("SELECT ALL column1 FROM table1", id="select_mode_all"),
     pytest.param("SELECT DISTINCT column1 FROM table1", id="select_mode_distinct"),
     pytest.param("SELECT column1, column2 FROM table1, table2", id="select_from_multi"),
     pytest.param("SELECT column1 FROM table1 AS alias1", id="select_from_alias_explicit"),
     pytest.param("SELECT column1 FROM table1 alias1", id="select_from_alias_implicit"),
+    pytest.param(
+        """
+        SELECT DISTINCT
+            * EXCEPT (column1, column2) REPLACE (column3 AS alias3, column4 AS alias4),
+            column5,
+            (SELECT 1) AS column6
+        FROM
+            table1 alias1,
+            (SELECT column7 FROM table2) AS alias2
+        """,
+        id="select_kitchen_sink"
+    ),
 ))
 def test_select_ok(sql, assert_parse_tree):
     assert_parse_tree(sql)
@@ -30,6 +42,7 @@ def test_select_ok(sql, assert_parse_tree):
     pytest.param("SELECT ,1", id="select_comma_leading"),
     # FIXME: BQ says "Syntax error: SELECT list must not be empty at [1:8]"
     pytest.param("SELECT from FROM table1", id="select_column_reserved_word", marks=pytest.mark.xfail),
+    pytest.param("SELECT * REPLACE (column1 alias1) FROM table1", id="select_star_replace_alias_implicit"),
 ))
 def test_select_error(sql, assert_parse_tree_error):
     assert_parse_tree_error(sql)
