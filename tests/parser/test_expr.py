cb4413c8c0d8daab54f14ef29ddb57746746ca2a
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-09 17:21:24 -0400

    feat: add subscript operator

--- tests/parser/test_expr.py
+++ tests/parser/test_expr.py
@@ -12,6 +12,17 @@ def test_dotted_expr_ok(sql, assert_parse_tree):
 
 
 @pytest.mark.parametrize("sql", (
+    pytest.param("SELECT column1[0]", id="subscript_expr_integer_literal_single"),
+    pytest.param("SELECT column1[0][1]", id="subscript_expr_integer_literal_multi"),
+    pytest.param("SELECT column1.column2[0][1]", id="subscript_expr_path_expr_nested"),
+    pytest.param("SELECT column1[OFFSET(2)]", id="subscript_expr_offset"),
+    pytest.param("SELECT column1['key1']", id="subscript_expr_string_literal"),
+))
+def test_subscript_expr_ok(sql, assert_parse_tree):
+    assert_parse_tree(sql)
+
+
+@pytest.mark.parametrize("sql", (
     pytest.param("SELECT FUNC()", id="fcall_pos_arg_none"),
     pytest.param("SELECT FUNC(1)", id="fcall_pos_arg_single"),
     pytest.param("SELECT FUNC(1, 2)", id="fcall_pos_arg_multi"),
