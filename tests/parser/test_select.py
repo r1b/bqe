1c61eafe4fc6f9e68df87f98ebd02c638183add1
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-10 22:13:02 -0400

    remove stuff that is technically unnecessary in pipe SQL

--- tests/parser/test_select.py
+++ tests/parser/test_select.py
@@ -11,22 +11,16 @@ import pytest
     pytest.param("SELECT * FROM table1", id="select_star"),
     pytest.param("SELECT column1.* FROM table1", id="select_star_scoped"),
     pytest.param("SELECT column1.column2.* FROM table1", id="select_star_scoped_nested"),
-    pytest.param("SELECT * EXCEPT (column1) FROM table1", id="select_star_except_single"),
-    pytest.param("SELECT * EXCEPT (column1, column2) FROM table1", id="select_star_except_multi"),
-    pytest.param("SELECT * REPLACE (column1 AS alias1) FROM table1", id="select_star_replace_single"),
-    pytest.param("SELECT * REPLACE (column1 AS alias1, column2 AS alias2) FROM table1", id="select_star_replace_multi"),
-    pytest.param("SELECT ALL column1 FROM table1", id="select_mode_all"),
-    pytest.param("SELECT DISTINCT column1 FROM table1", id="select_mode_distinct"),
     pytest.param("SELECT column1, column2 FROM table1, table2", id="select_from_multi"),
     pytest.param("SELECT column1 FROM table1 AS alias1", id="select_from_alias_explicit"),
     pytest.param("SELECT column1 FROM table1 alias1", id="select_from_alias_implicit"),
     pytest.param("SELECT column1 FROM table1 FOR SYSTEM_TIME AS OF '1970-01-01'", id="select_from_time_travel"),
     pytest.param(
         """
-        SELECT DISTINCT
-            * EXCEPT (column1, column2) REPLACE (column3 AS alias3, column4 AS alias4),
-            column5,
-            (SELECT 1) AS column6
+        SELECT
+            *,
+            column1,
+            (SELECT 1) AS column2
         FROM
             table1 alias1,
             (SELECT column7 FROM table2) AS alias2
