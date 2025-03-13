c9bb6ac6061b9b8bb404615fe6694de896e23253
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-12 20:44:19 -0400

    refactor: extend to bqe, support scripts

--- bqe/parser.py
+++ bqe/parser.py
@@ -2,8 +2,10 @@ from pathlib import Path
 from lark import Lark
 from .settings import PACKAGE_ROOT
 
-GRAMMAR_PATH = PACKAGE_ROOT / Path("grammar/bigquery.lark")
+GRAMMAR_ROOT = PACKAGE_ROOT / "grammar"
+GRAMMAR_PATH = GRAMMAR_ROOT / "bqe.lark"
+GRAMMAR_SOURCES = GRAMMAR_ROOT / "src"
 
 
 def get_parser() -> Lark:
-    return Lark(GRAMMAR_PATH.read_text(), parser='lalr')
+    return Lark(GRAMMAR_PATH.read_text(), import_paths=[GRAMMAR_SOURCES], parser='lalr')
