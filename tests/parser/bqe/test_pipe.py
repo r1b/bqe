029b070998b31226a35f9cb28403c5de9d932c58
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-15 16:07:03 -0400

    feat: support agg, group by and grouping sets

--- tests/parser/bqe/test_pipe.py
+++ tests/parser/bqe/test_pipe.py
@@ -81,3 +81,24 @@ def test_where_ok(sql, assert_parse_tree):
 ))
 def test_limit_ok(sql, assert_parse_tree):
     assert_parse_tree(sql)
+
+
+@pytest.mark.parametrize("sql", (
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1)", id="pipe_agg_single"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2", id="pipe_agg_single_alias"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2, SUM(col3) AS col4", id="pipe_agg_multi_alias"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2 GROUP BY col3", id="pipe_agg_group_by_single"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2 GROUP BY col3 AS col4", id="pipe_agg_group_by_single_alias"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2 GROUP BY col3 AS col4, col5 AS col6",
+                 id="pipe_agg_group_by_multi_alias"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2 GROUP BY ()",
+                 id="pipe_agg_group_by_empty"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2 GROUP BY ROLLUP (col3, col4)",
+                 id="pipe_agg_group_by_rollup"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2 GROUP BY CUBE (col3, (col4, col5))",
+                 id="pipe_agg_group_by_cube"),
+    pytest.param("FROM table1 |> AGGREGATE SUM(col1) AS col2 GROUP BY GROUPING SETS (col3, (col4, col5), col6)",
+                 id="pipe_agg_group_by_grouping_sets"),
+))
+def test_agg_ok(sql, assert_parse_tree):
+    assert_parse_tree(sql)
