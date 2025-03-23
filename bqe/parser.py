edcc14516b9822e60f30982ba4724b786f01db8b
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-23 14:04:02 -0400

    refactor: merge grammars

--- bqe/parser.py
+++ bqe/parser.py
@@ -4,8 +4,7 @@ from .settings import PACKAGE_ROOT
 
 GRAMMAR_ROOT = PACKAGE_ROOT / "grammar"
 GRAMMAR_PATH = GRAMMAR_ROOT / "bqe.lark"
-GRAMMAR_SOURCES = GRAMMAR_ROOT / "src"
 
 
 def get_parser() -> Lark:
-    return Lark(GRAMMAR_PATH.read_text(), import_paths=[GRAMMAR_SOURCES], parser='lalr')
+    return Lark(GRAMMAR_PATH.read_text(), parser='lalr')
