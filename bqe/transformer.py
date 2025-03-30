ef9662bd15d47acb85c5362c66bf4211e7959c6a
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-30 15:21:07 -0400

    feat: rewrite path_expression to ident

--- bqe/transformer.py
+++ bqe/transformer.py
@@ -2,7 +2,15 @@ from lark import Transformer, Tree
 
 
 class BqeTransformer(Transformer):
-    def field_access_expr(self, tree: Tree):
-        """Rewrites left-recursive paths to ident"""
-        lhs, rhs = tree
-        return Tree('ident', lhs.children + rhs.children)
+    def field_access_expr(self, args: list[Tree]):
+        """Rewrites left-recursive field_access_expr to ident"""
+        parent, child = args
+        lhs, rhs = parent.children, child.children
+        return Tree('ident', lhs + rhs)
+
+    def path_expression(self, args: list[Tree]):
+        """Rewrites left-recursive path_expression to ident"""
+        next_args = [Tree('_noop', [])] + args
+        parent, child = next_args[-2:]
+        lhs, rhs = parent.children, child.children
+        return Tree('ident', lhs + rhs)
