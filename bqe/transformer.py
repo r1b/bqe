f9121fb4e66751e63bde3426f74a5ea7ae8e9498
Robert Jensen <robert.cole.jensen@gmail.com>
2025-03-30 21:48:27 -0400

    feat: fixup named struct literal tree

--- bqe/transformer.py
+++ bqe/transformer.py
@@ -1,15 +1,17 @@
 from lark import Transformer, Tree
 
 
-def fixup_lrec_binary(args: list[Tree], name: str) -> Tree:
-    """Fixup left-recursive binary trees. Returns a Tree named `name` with an n-ary list of children."""
+def fixup_lrec_rule(args: list[Tree], name: str, spread=True) -> Tree:
+    """Fixup left-recursive rules. Returns a Tree named `name` with a list of all leaves collected
+    from the children of the RHS. If `spread` is `False`, the RHS will be preserved along with its children.
+    """
     parent, child = args
-    lhs, rhs = parent.children, child.children
+    lhs, rhs = parent.children, child.children if spread else [child]
     return Tree(name, lhs + rhs)
 
 
 def coerce_binary(args: list[Tree]) -> list[Tree]:
-    """Push a dummy node to guarantee that there are exactly two nodes in args."""
+    """Push a dummy node to guarantee that there are exactly two nodes in `args`."""
     next_args = [Tree("_noop", [])] + args
     return next_args[-2:]
 
@@ -17,20 +19,24 @@ def coerce_binary(args: list[Tree]) -> list[Tree]:
 class BqeTransformer(Transformer):
     def field_access_expr(self, args: list[Tree]):
         """Rewrite left-recursive field_access_expr to ident"""
-        return fixup_lrec_binary(args, "ident")
+        return fixup_lrec_rule(args, "ident")
 
     def path_expression(self, args: list[Tree]):
         """Rewrite left-recursive path_expression to ident"""
-        return fixup_lrec_binary(coerce_binary(args), "ident")
+        return fixup_lrec_rule(coerce_binary(args), "ident")
 
     def grouping_set_list(self, args: list[Tree]):
         """Collapse left-recursive grouping_set_list"""
-        return fixup_lrec_binary(coerce_binary(args), "grouping_set_list")
+        return fixup_lrec_rule(coerce_binary(args), "grouping_set_list")
 
     def cube_list(self, args: list[Tree]):
         """Collapse left-recursive cube_list"""
-        return fixup_lrec_binary(coerce_binary(args), "cube_list")
+        return fixup_lrec_rule(coerce_binary(args), "cube_list")
 
     def rollup_list(self, args: list[Tree]):
         """Collapse left-recursive rollup_list"""
-        return fixup_lrec_binary(coerce_binary(args), "rollup_list")
+        return fixup_lrec_rule(coerce_binary(args), "rollup_list")
+
+    def named_struct_literal_item_list(self, args: list[Tree]):
+        """Collapse left-recursive named_struct_literal_item_list"""
+        return fixup_lrec_rule(coerce_binary(args), "named_struct_literal_item_list", spread=False)
