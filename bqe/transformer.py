d3b8eafba31a76e6859cc9f788165fbee56eb0c7
Robert Jensen <robert.cole.jensen@gmail.com>
2025-04-04 17:29:03 -0400

    refactor: inline transformer logic, dont spread

--- bqe/transformer.py
+++ bqe/transformer.py
@@ -1,21 +1,6 @@
 from lark import Transformer, Tree
 
 
-def fixup_lrec_rule(args: list[Tree], name: str, spread=True) -> Tree:
-    """Fixup left-recursive rules. Returns a Tree named `name` with a list of all leaves collected
-    from the children of the RHS. If `spread` is `False`, the RHS will be preserved along with its children.
-    """
-    parent, child = args
-    lhs, rhs = parent.children, child.children if spread else [child]
-    return Tree(name, lhs + rhs)
-
-
-def coerce_binary(args: list[Tree]) -> list[Tree]:
-    """Push a dummy node to guarantee that there are exactly two nodes in `args`."""
-    next_args = [Tree("_noop", [])] + args
-    return next_args[-2:]
-
-
 class BqeTransformer(Transformer):
     def ident(self, args: list[Tree]):
         """Rewrite keyword_as_ident to ident"""
@@ -26,49 +11,76 @@ class BqeTransformer(Transformer):
 
     def query_expr(self, args: list[Tree]):
         """Collapse left-recursive query_expr"""
-        return fixup_lrec_rule(coerce_binary(args), "query_expr", spread=False)
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("query_expr", lhs + rhs)
+        return Tree("query_expr", args)
 
     def path_expression(self, args: list[Tree]):
-        """Rewrite left-recursive path_expression"""
-        return fixup_lrec_rule(coerce_binary(args), "path_expression")
+        """Collapse left-recursive path_expression"""
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("path_expression", lhs + rhs)
+        return Tree("path_expression", args)
 
     def field_access_expr(self, args: list[Tree]):
         """Rewrite left-recursive field_access_expr to path_expression.
         When a non-ident lhs is encountered, rewrite to dot_ident."""
         parent, child = args
-
         if parent.data == "ident":
             return Tree("path_expression", args)
-
         if parent.data in ("field_access_expr", "path_expression"):
             lhs, rhs = parent.children, [child]
             children = lhs + rhs
             return Tree("path_expression", children)
-
         return Tree("dot_ident", args)
 
     def grouping_set_list(self, args: list[Tree]):
         """Collapse left-recursive grouping_set_list"""
-        return fixup_lrec_rule(coerce_binary(args), "grouping_set_list")
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("grouping_set_list", lhs + rhs)
+        return Tree("grouping_set_list", args)
 
     def cube_list(self, args: list[Tree]):
         """Collapse left-recursive cube_list"""
-        return fixup_lrec_rule(coerce_binary(args), "cube_list")
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("cube_list", lhs + rhs)
+        return Tree("cube_list", args)
 
     def rollup_list(self, args: list[Tree]):
         """Collapse left-recursive rollup_list"""
-        return fixup_lrec_rule(coerce_binary(args), "rollup_list")
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("rollup_list", lhs + rhs)
+        return Tree("rollup_list", args)
 
     def named_struct_literal_item_list(self, args: list[Tree]):
         """Collapse left-recursive named_struct_literal_item_list"""
-        return fixup_lrec_rule(coerce_binary(args), "named_struct_literal_item_list", spread=False)
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("named_struct_literal_item_list", lhs + rhs)
+        return Tree("named_struct_literal_item_list", args)
 
     def pipe_selection_item_list(self, args: list[Tree]):
         """Collapse left-recursive pipe_selection_item_list"""
-        return fixup_lrec_rule(coerce_binary(args), "pipe_selection_item_list", spread=False)
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("pipe_selection_item_list", lhs + rhs)
+        return Tree("pipe_selection_item_list", args)
 
     def pipe_selection_item_list_with_order(self, args: list[Tree]):
         """Collapse left-recursive pipe_selection_item_list_with_order"""
-        return fixup_lrec_rule(
-            coerce_binary(args), "pipe_selection_item_list_with_order", spread=False
-        )
+        if len(args) == 2:
+            parent, child = args
+            lhs, rhs = parent.children, [child]
+            return Tree("pipe_selection_item_list_with_order", lhs + rhs)
+        return Tree("pipe_selection_item_list_with_order", args)
